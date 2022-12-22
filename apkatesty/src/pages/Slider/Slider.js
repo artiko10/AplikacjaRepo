4cfc6de40bdb574405b20c06aed8f76dd5b02258
artiko10 <artur.gomulka11@gmail.com>
2022-12-22 22:17:47 +0100

    dodanie slidera volume

--- apkatesty/src/pages/Slider/Slider.js
+++ apkatesty/src/pages/Slider/Slider.js
@@ -1,34 +1,26 @@
-import { useEffect, useState } from "react";
+import * as React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './Slider.css';
 import Form from 'react-bootstrap/Form';
 import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 import Slider from '@mui/material/Slider';
+import VolumeDown from '@mui/icons-material/VolumeDown';
+import VolumeUp from '@mui/icons-material/VolumeUp';
+
+
 
 function Sliders() {
 
-    const [data,setData]=useState(0)
-    const [grade,setGrade]=useState('1')
+  const [value, setValue] = React.useState(30);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   
-    const [begining,setbegin]=useState(0)
-    const [ending,setending]=useState(51)
-
-    useEffect(()=>{
-        if(data == 0){
-          setGrade("1")
-        }else if(data == 30){
-          setGrade("2")
-        }else if(data == 50){
-          setGrade("3")
-        }else if(data == 70){
-          setGrade("4")
-        }else if(data == 90){
-          setGrade("5")
-        }else if(data == 100){
-          setGrade("6")
-        }
-      },[data])
-
+  
+ 
 return (
     <>
 
@@ -45,7 +37,7 @@ return (
     <div class="col-sm-7"><div class="p-5 border rounded bg-success tasks text-light">
       <h3 class="font-weight-bold text-uppercase text-center">Tasks</h3>
 
-      <h5 class="text-light my-5">1. Check the percentage range for the grade</h5>
+      <h5 class="text-light my-5">1. Change range to 20</h5>
         
       <Box width={300}>
       <Slider
@@ -54,19 +46,20 @@ return (
         aria-label="Small"
         valueLabelDisplay="auto"
       />
-      <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
+      
+    </Box>
+
+    <h5 class="text-light my-5">2. Set volume to 80%</h5>
+    <Box sx={{ width: 200 }}>
+      <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+        <VolumeDown />
+        <Slider aria-label="Volume" value={value} onChange={handleChange} />
+        <VolumeUp />
+      </Stack>
+      
     </Box>
         
-          <div>
-            <Form.Range id="SliderGrade" type='range' min='0' max="100" step="1" value={data} onChange={(e)=>setData(e.target.value)}/>
-            <h5 id="SliderData">{data} %</h5>
-            <h5 id="SliderGrade" className="text-danger">Grade: {grade}</h5>
-          </div>
-          <h5 class="text-light my-5">2. Check the minimum and maximum range for both sliders </h5>
-          <Form.Range id="SliderFirstHalf" type='range' min='0' max="50" step="1" value={begining} onChange={(e)=>setbegin(e.target.value)}/>
-            <h5 id="FirstHalfData">from: {begining}</h5>
-            <Form.Range id="SliderSecondHalf" type='range' min='51' max="100" step="1" value={ending} onChange={(e)=>setending(e.target.value)}/>
-            <h5 id="SecondHalfData">to: {ending}</h5>
+         
             <h5 class="text-light my-5">3. Check if this slider is disabled</h5>
             <label for="disabledRange" class="form-label">Disabled range</label>
             <input type="range" class="form-range" id="disabledRange" disabled></input>
